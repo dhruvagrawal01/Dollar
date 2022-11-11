80c1b5c37ddcf96395aa790ee0611d4fd212ff9c
Pranav Burugula <pranav.burugula@gmail.com>
2022-11-11 11:30:04 -0600

    Initial Commit

---
+++ Dollar/DollarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollarApp.swift
+//  Dollar
+//
+//  Created by Pranav Burugula on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DollarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
