2f543043d8c8ae2b100c83599fa31bf72da79e78
Rohan Vasavada <rohanvasavada@gmail.com>
2022-11-15 14:04:57 -0600

    added reimbursement request form

---
+++ Dollar/ReimbursementView.swift
@@ -0,0 +1,58 @@
+//
+//  ReimbursementView.swift
+//  Dollar
+//
+//  Created by Rohan Vasavada on 11/15/22.
+//
+
+import SwiftUI
+
+struct ReimbursementView: View {
+	@Binding var page_index: Int
+	@State private var title: String = ""
+	
+	var body: some View {
+		Form {
+			VStack(
+				alignment: .leading,
+				spacing: 10
+			) {
+				Text("Reimbursement Request").font(.title)
+				
+				Spacer()
+				
+				HStack() {
+					Text("Title")
+					TextField("", text: $title, prompt: Text("Request Title"))
+				}
+				
+				Spacer()
+				
+				HStack() {
+					Text("Amount")
+					TextField("", text: $title, prompt: Text("Request Amount"))
+				}
+				
+				Spacer()
+				
+				HStack() {
+					Text("Description")
+					TextField("", text: $title, prompt: Text("Request Description"), axis: .vertical).lineLimit(5...10)
+				}
+				
+				Spacer()
+
+				Button("Submit") {
+					page_index = 0
+				}
+			}.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+		}.textFieldStyle(.roundedBorder)
+	}
+	
+}
+
+struct ReimbursementView_Previews: PreviewProvider {
+	static var previews: some View {
+		ReimbursementView(page_index: .constant(1))
+	}
+}
